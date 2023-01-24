d02e638e042b0cbe8f1e4b825a20da6b0fb59d73
Maha <maha@gokepler.fr>
2023-01-24 10:39:03 +0100

    Add admins listing

---
+++ onfleet/admin.go
@@ -0,0 +1,25 @@
+package onfleet
+
+import "context"
+
+type AdminsService service
+
+type Admin struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (s *AdminsService) List(ctx context.Context) ([]Admin, error) {
+	req, err := s.client.NewRequest("GET", "admins", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var admins []Admin
+	err = s.client.Do(ctx, req, &admins)
+	if err != nil {
+		return nil, err
+	}
+
+	return admins, nil
+}
